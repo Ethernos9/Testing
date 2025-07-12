18a711bcd74d2815f9f3987374b35edca2e8800b
Shubham Panse <142664433+Ethernos9@users.noreply.github.com>
2025-07-12 11:37:19 +0530

    Update sum.test.ts

--- 1-simple-test/src/tests/sum.test.ts
+++ 1-simple-test/src/tests/sum.test.ts
@@ -7,7 +7,7 @@ describe('sum module', () => {
   });
 });
 describe("multiply module checks the multiply",()=>{
-  test("ahould check multiplication of 1 And 2",()=>{
+  test("should check multiplication of 1 And 2",()=>{
     const finalAnswer =mul(3,2)
     expect(finalAnswer).toBe(6) 
   })
